636b6f2e4ed24096dc78d17267e9377b75af5849
AgostaSalvatore <agosta.boolean@gmail.com>
2025-07-18 13:33:09 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+<form>
+    <h1>Generatore Password</h1>
+    <hr>
+    <div class="container">
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Genera Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <button>Submit</button>
+    </div>
+</form>
+</body>
+</html>
